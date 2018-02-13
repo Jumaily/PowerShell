91318faa9d5c91b3d35c2191859a6cf89879639f
Al Jumaily <Jumaily@users.noreply.github.com>
2018-02-13 15:10:22 -0500

    Get installed software version
    Windows powershell - get list of specific software & version from list of workstation (flat file)

---
+++ Get_Installed_Software_Info.ps1
@@ -0,0 +1,12 @@
+﻿$app1 = 'McKesson Radiology Station'
+$app2 = 'McKesson Radiology Station Force Logoff'
+$OutputFile = "Radio_App_Version.txt"
+
+Get-Content .\WorkStations.txt | ForEach {
+   echo "Getting info for $_ "
+   $a = Get-WmiObject Win32_Product -Filter "Name='$app1'" -ComputerName $_
+   $b = Get-WmiObject Win32_Product -Filter "Name='$app2'" -ComputerName $_
+   echo "$_" >> $OutputFile
+   $a >> $OutputFile
+   $b >> $OutputFile
+   }
